c5406f78507250bcf98e730d31e799f16f56527b
amitbansal26 <amitbansal26@gmail.com>
2022-11-17 19:48:32 +0530

    Initial commit

---
+++ src/main/java/com/amit/demo/saga/sagatransaction/SagatransactionApplication.java
@@ -0,0 +1,13 @@
+package com.amit.demo.saga.sagatransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagatransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagatransactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amit/demo/saga/sagatransaction/SagatransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amit.demo.saga.sagatransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagatransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
